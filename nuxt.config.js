c87557cac82ac5c302162a12cb7cde4a4e4c2bde
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-09-27 15:03:50 +0200

    ajout des polices

--- nuxt.config.js
+++ nuxt.config.js
@@ -3,7 +3,7 @@ export default {
   head: {
     title: 'bypass',
     htmlAttrs: {
-      lang: 'en'
+      lang: 'fr'
     },
     meta: [
       { charset: 'utf-8' },
@@ -12,7 +12,10 @@ export default {
       { name: 'format-detection', content: 'telephone=no' }
     ],
     link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
+      { rel: "preconnect", href: "https://fonts.googleapis.com" },
+      { rel: "preconnect", href: "https://fonts.gstatic.com" },
+      {rel: "stylesheet", href: "https://fonts.googleapis.com/css2?family=Bree+Serif&family=Open+Sans&display=swap" }
     ]
   },
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,8 @@
 <template>
-	<p> bjr </p>
+	<div>
+		<h1>Bypass</h1>
+		<p>Salut à tous on se retrouve pour le meilleur workshop le plus éclaté du monde et de la galaxie</p>
+	</div>
 </template>
 
 <script>
