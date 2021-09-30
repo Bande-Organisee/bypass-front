d43cf973bdcea20bdf7d6885799a56f6d9639ac0
Théo Migeat <44432694+STM3900@users.noreply.github.com>
2021-09-30 10:43:52 +0200

    https

--- components/Form.vue
+++ components/Form.vue
@@ -36,7 +36,7 @@ export default {
       // 8006
       console.log("scan");
       axios
-        .get(`http://workshop.sbcorp.ovh:3842/getIp?ip=${ip}&port=${+port}`)
+        .get(`https://workshop.sbcorp.ovh:3842/getIp?ip=${ip}&port=${+port}`)
         .then(response => (this.response = response.data));
     }
   }
